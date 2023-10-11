34458d636bdfbcb596427e7cbafe00d9706912e6
Kartik <116331949+Krmovierequest@users.noreply.github.com>
2023-10-11 21:01:32 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Rokstar890/Newbot.git /Newbot
+  git clone https://github.com/Krmovierequest/Newbot.git /Newbot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Newbot
