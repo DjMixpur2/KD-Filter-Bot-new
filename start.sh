fb7dba4b19f6fca9deb7a91ae61da304e9eab1ba
Kartik <116331949+Krmovierequest@users.noreply.github.com>
2023-10-12 21:19:11 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Krmovierequest/Newbot.git /Newbot
+  git clone https://github.com/Krmovierequest/KD-Filter-Bot-new.git /Newbot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Newbot
+  git clone $UPSTREAM_REPO /KD-Filter-Bot-new
 fi
-cd /Newbot
+cd /KD-Filter-Bot-new
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
